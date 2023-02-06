973a3631c99682f52462d0e1c2ba3869d968475f
matthijsvanderplas <mvdplas84@ziggo.nl>
2023-02-06 20:46:21 +0100

    Set up mongoDB connection and Schema

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require('dotenv').config()
 const port = 3000
 
 // middleware
+app.use(express.static('./public'))
 app.use(express.json())
 
 // routes
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,28 +1,66 @@
 const Task = require('../models/task')
 
-const getAllTasks = (req, res) => {
-  res.send('All Tasks')
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find()
+    res.status(200).json({tasks})
+  } catch (error) {
+    res.status(500).json({msg: error.message})
+  }
 }
 
 const createTask = async (req, res) => {
   try {
     const task = await Task.create(req.body)
     res.status(201).json({ task })
-  } catch (err) {
-    console.log(err)
+  } catch (error) {
+    res.status(500).json({msg: error.message})
   }
 }
 
-const getTask = (req, res) => {
-  res.send('Single Task')
+const getTask = async (req, res) => {
+  try {
+    const {id:taskID} = req.params
+    const task = await Task.findOne({ _id: taskID })
+
+    if (!task) {
+      return res.status(404).json({msg: `No task with id : ${taskID}`})
+    }
+
+    res.status(200).json({task})
+  } catch (error) {
+    res.status(500).json({msg: error.message})
+  }
 }
 
-const updateTask = (req, res) => {
-  res.send('Update Task')
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOneAndDelete({_id: taskID})
+    if (!task) {
+      return res.status(404).json({msg: `No task with id : ${taskID}`})
+    }
+    res.status(200).json({task})
+  } catch (error) {
+    res.status(500).json({msg: error.message})
+  }
 }
 
-const deleteTask = (req, res) => {
-  res.send('Delete Task')
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findByIdAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true
+    })
+    if (!task) {
+      return res.status(404).json({msg: `No task with id : ${taskID}`})
+    }
+
+    res.status(200).json({task})
+  } catch (error) {
+    res.status(500).json({msg: error.message})
+  }
 }
 
 module.exports = {
